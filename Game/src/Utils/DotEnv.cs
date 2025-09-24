0f9fe86f6db9e7238f1020000189c80bed28e6bc
clement lion <clement.lionceau@gmail.com>
2025-09-24 19:06:41 +0200

    reformating

--- Game/src/Utils/DotEnv.cs
+++ Game/src/Utils/DotEnv.cs
@@ -3,6 +3,7 @@ using DotNetEnv;
 public class DotNetVariables
 {
     public int ServerPort => int.Parse(Environment.GetEnvironmentVariable("API_KEY") ?? "5000");
+
     public DotNetVariables()
     {
         ServiceLocator.Register<DotNetVariables>(this);
